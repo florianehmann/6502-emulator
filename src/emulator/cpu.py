b82473c92e03b6e4658dbe630aabe5566a3a2185
Florian Ehmann <kontakt@florianehmann.de>
2026-01-22 21:36:13 +0100

    More addressing modes

--- src/emulator/cpu.py
+++ src/emulator/cpu.py
@@ -18,6 +18,8 @@ class AddressingMode(enum.Enum):
     ZERO_PAGE_X = enum.auto()
     ABSOLUTE = enum.auto()
     ABSOLUTE_X = enum.auto()
+    ABSOLUTE_Y = enum.auto()
+    INDIRECT_X = enum.auto()
 
 
 class CPU6502:
@@ -30,7 +32,7 @@ class CPU6502:
         self.x: int = 0
         self.y: int = 0
         self.pc: int = 0
-        self.sp: int = 0xfd
+        self.sp: int = 0xff
         self.status: int = 0
         self.cycles: int = 0
 
@@ -40,10 +42,12 @@ class CPU6502:
     def build_opcode_table(self) -> dict[int, Callable[[], None]]:
         """Return a map between opcode and method that contains the logic for the instruction."""
         return {
+            0xa1: partial(self.lda, mode=AddressingMode.INDIRECT_X),
             0xa5: partial(self.lda, mode=AddressingMode.ZERO_PAGE),
             0xad: partial(self.lda, mode=AddressingMode.ABSOLUTE),
             0xa9: partial(self.lda, mode=AddressingMode.IMMEDIATE),
             0xb5: partial(self.lda, mode=AddressingMode.ZERO_PAGE_X),
+            0xb9: partial(self.lda, mode=AddressingMode.ABSOLUTE_Y),
             0xbd: partial(self.lda, mode=AddressingMode.ABSOLUTE_X),
         }
 
@@ -92,7 +96,24 @@ class CPU6502:
                 self.a = self.memory.read((addr + self.x) & 0xffff)
                 self.pc += 2
                 self.cycles += 4 if (addr & 0xff00) == ((addr + self.x) & 0xff00) else 5
+            case AddressingMode.ABSOLUTE_Y:
+                addr_lo = self.memory.read(self.pc)
+                addr_hi = self.memory.read(self.pc + 1)
+                addr = (addr_hi << 8) | addr_lo
+                self.a = self.memory.read((addr + self.y) & 0xffff)
+                self.pc += 2
+                self.cycles += 4 if (addr & 0xff00) == ((addr + self.y) & 0xff00) else 5
+            case AddressingMode.INDIRECT_X:
+                addr_zp = (self.memory.read(self.pc) + self.x) & 0xff
+                addr_indirect_lo = self.memory.read(addr_zp)
+                addr_indirect_hi = self.memory.read((addr_zp + 1) & 0xff)
+                addr_indirect = (addr_indirect_hi << 8) | addr_indirect_lo
+                self.a = self.memory.read(addr_indirect)
+                self.pc += 1
+                self.cycles += 6
             case _:
-                logger.warning(f"Invalid addressing mode {mode.name} for LDA instruction.")
+                # this should not ever be reached when running because the
+                # opcode table should never contain an invalid call
+                logger.error(f"Invalid addressing mode {mode.name} for LDA instruction.")
 
         # TODO: Update status register
--- tests/unit/test_cpu.py
+++ tests/unit/test_cpu.py
@@ -9,6 +9,8 @@ INDEX: int = 0x05
 ABSOLUTE_LOCATION: int = 0x0101  # lower byte + index must be less than 255
 PAGE_CROSS_ABSOLUTE_LOCATION: int = 0x0101
 PAGE_CROSS_INDEX: int = 0xff
+POINTER_LOCATION: int = 0x0008
+INDIRECT_DATA_LOCATION: int = 0x0010
 
 @pytest.mark.parametrize(
     ('mode', 'page_boundary_cross'),
@@ -19,15 +21,20 @@ PAGE_CROSS_INDEX: int = 0xff
         (AddressingMode.ABSOLUTE, False),
         (AddressingMode.ABSOLUTE_X, False),
         (AddressingMode.ABSOLUTE_X, True),
+        (AddressingMode.ABSOLUTE_Y, False),
+        (AddressingMode.ABSOLUTE_Y, True),
+        (AddressingMode.INDIRECT_X, False),
     ],
 )
-def test_lda_addressing(cpu: CPU6502, mode: AddressingMode, page_boundary_cross: bool):  # noqa: D103, FBT001
+def test_lda_addressing(cpu: CPU6502, mode: AddressingMode, page_boundary_cross: bool):  # noqa: D103, FBT001, PLR0915
     test_value = {
         AddressingMode.IMMEDIATE: 0x01,
         AddressingMode.ZERO_PAGE: 0x02,
         AddressingMode.ZERO_PAGE_X: 0x03,
         AddressingMode.ABSOLUTE: 0x04,
         AddressingMode.ABSOLUTE_X: 0x05,
+        AddressingMode.ABSOLUTE_Y: 0x06,
+        AddressingMode.INDIRECT_X: 0x07,
     }
     opcode = {
         AddressingMode.IMMEDIATE: 0xa9,
@@ -35,6 +42,8 @@ def test_lda_addressing(cpu: CPU6502, mode: AddressingMode, page_boundary_cross:
         AddressingMode.ZERO_PAGE_X: 0xb5,
         AddressingMode.ABSOLUTE: 0xad,
         AddressingMode.ABSOLUTE_X: 0xbd,
+        AddressingMode.ABSOLUTE_Y: 0xb9,
+        AddressingMode.INDIRECT_X: 0xa1,
     }
     cycles = {
         AddressingMode.IMMEDIATE: 2,
@@ -42,6 +51,8 @@ def test_lda_addressing(cpu: CPU6502, mode: AddressingMode, page_boundary_cross:
         AddressingMode.ZERO_PAGE_X: 4,
         AddressingMode.ABSOLUTE: 4,
         AddressingMode.ABSOLUTE_X: 4,  # no page boundary cross
+        AddressingMode.ABSOLUTE_Y: 4,  # no page boundary cross
+        AddressingMode.INDIRECT_X: 6,
     }
     pc_after = {
         AddressingMode.IMMEDIATE: 2,
@@ -49,8 +60,10 @@ def test_lda_addressing(cpu: CPU6502, mode: AddressingMode, page_boundary_cross:
         AddressingMode.ZERO_PAGE_X: 2,
         AddressingMode.ABSOLUTE: 3,
         AddressingMode.ABSOLUTE_X: 3,
+        AddressingMode.ABSOLUTE_Y: 3,
+        AddressingMode.INDIRECT_X: 2,
     }
-    page_cross_extra_cycles = [AddressingMode.ABSOLUTE_X]
+    page_cross_extra_cycles = [AddressingMode.ABSOLUTE_X, AddressingMode.ABSOLUTE_Y]
 
     cpu.pc = 0
     cpu.memory.write(0, opcode[mode])
@@ -84,6 +97,26 @@ def test_lda_addressing(cpu: CPU6502, mode: AddressingMode, page_boundary_cross:
             cpu.memory.write(effective_address, test_value[mode])
             cpu.memory.write(1, address_lo)
             cpu.memory.write(2, address_hi)
+        case AddressingMode.ABSOLUTE_Y:
+            if page_boundary_cross:
+                cpu.y = PAGE_CROSS_INDEX
+                absolute_location = PAGE_CROSS_ABSOLUTE_LOCATION
+                effective_address = PAGE_CROSS_ABSOLUTE_LOCATION + PAGE_CROSS_INDEX
+            else:
+                cpu.y = INDEX
+                absolute_location = ABSOLUTE_LOCATION
+                effective_address = ABSOLUTE_LOCATION + INDEX
+            address_lo = absolute_location & 0xff
+            address_hi = (absolute_location >> 8) & 0xff
+            cpu.memory.write(effective_address, test_value[mode])
+            cpu.memory.write(1, address_lo)
+            cpu.memory.write(2, address_hi)
+        case AddressingMode.INDIRECT_X:
+            cpu.x = INDEX
+            cpu.memory.write(1, POINTER_LOCATION - INDEX)
+            cpu.memory.write(POINTER_LOCATION, INDIRECT_DATA_LOCATION & 0xff)
+            cpu.memory.write(POINTER_LOCATION + 1, (INDIRECT_DATA_LOCATION >> 8) & 0xff)
+            cpu.memory.write(INDIRECT_DATA_LOCATION, test_value[mode])
         case _:
             raise ValueError
     cpu.step()
